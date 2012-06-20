4f952500997000cb804cee5b8aedcff69168ff4a
denniswlin <27155477@qq.com>
2012-06-20 18:32:03 +0800

    lib

---
+++ WebContent/js/a.js
@@ -0,0 +1,5 @@
+define("#validator/0.8.1/core-debug", ["$","./async","widget","./utils","./item","./rule"], function(require, exports, module) {
+
+var ss = v.a;
+	
+});
---
+++ WebContent/js/b.js
@@ -0,0 +1,10 @@
+define(function(require, exports) {
+
+	exports.init = function(){
+		var v = v || {};
+		v = {'a':'10'};
+		return v;	
+	};
+
+
+});
